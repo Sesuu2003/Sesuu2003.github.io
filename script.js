78dc6ede6c2d1379dc36f80effcf22554f790f56
Sesuu2003 <ulisescheveste@gmail.com>
2023-03-04 13:55:52 -0300

    Comenzando el script

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+const btnsuma = document.querySelector("#btnsuma");
+const btnresta = document.querySelector("#btnresta");
+const btnmult = document.querySelector("#btnmult");
+const btndiv = document.querySelector("#btndiv");
+const btnparen1 = document.querySelector("#btnparen1");
+const btnparen2 = document.querySelector("#btnparen2");
+const btnigual = document.querySelector("#btnigual");
+let textbox = document.querySelector("#textbox");
+let operacion;
+
+
+textbox.innerText = 2;
+
+
+
